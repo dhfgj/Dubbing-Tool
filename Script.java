189351212b1bdf3cc9a52d7760128d1b8e566b65
Dallos <maheshsullivan@gmail.com>
2015-03-14 17:31:12 -0400

    Update Script.java

--- Script.java
+++ Script.java
@@ -1,102 +1,84 @@
 import java.util.ArrayList;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Script {
-	private ArrayList<Track> tracks;
-	private String name;
-	private String path;
+    private ArrayList<Track> tracks;
+    private String name;
+    private String path;
+    private OffsetTree myTree;
+    public Script(String scriptName,String thePath){
+        name=scriptName;
+        path=thePath;
+        tracks=new ArrayList<Track>();
+    }
 
-	public Script(String scriptName,String thePath){
-		name=scriptName;
-		path=thePath;
-		tracks=new ArrayList<Track>();
-	}
+    public void setName(String theName){name=theName;}
+    public String getName(){return name;}
+    public ArrayList<Track> getScriptTracks(){return tracks;}
 
-	public void setName(String theName){name=theName;}
-	public String getName(){return name;}
-	public ArrayList<Track> getScriptTracks(){return tracks;}
+    public void deleteTrack(Track track){
+        if(track.getRelativeTo()==track){
+            boolean done=false;
+            int i;
+            for(i=1;!done&&i<tracks.size();i++){
+                if(tracks.get(i).getRelativeTo()!=track){
+                    done=true;
 
-	public void addTrack(Track track){
-		tracks.add(track);
-		//gots to sort bois
-	}
-	public void deleteTrack(Track track){
-		if(track.getRelativeTo()==track){
-			boolean done=false;
-			int i;
-			for(i=1;!done&&i<tracks.size();i++){
-				if(tracks.get(i).getRelativeTo()!=track){
-					done=true;
+                }
+            }
+            for(int j=1;j<i&&j<tracks.size();j++){
+                tracks.get(j).setRelativeTo(tracks.get(1));
+            }
+            tracks.remove(track);
+        }else{
+            tracks.remove(track);
+        }
+    }
 
-				}
-			}
-			for(int j=1;j<i&&j<tracks.size();j++){
-				tracks.get(j).setRelativeTo(tracks.get(1));
-			}
-			tracks.remove(track);
-		}else{
-			tracks.remove(track);
-		}
-	}
-	public int getDuration(){}
+    public void addTrack(Track theNewTrack) throws Exception{
+        if(theNewTrack.getRelativeTo()==theNewTrack){
+            if(tracks.size()==0){
+                tracks.add(theNewTrack);
+		myTree=new OffsetTree(theNewTrack);
+            }else{
+                throw new Exception();
+            }
+        }else if(theNewTrack.getRelativeTo()!=null){
+            int index1=tracks.indexOf(theNewTrack.getRelativeTo());
+            int index2=0;
+            for(int i=index1;i<tracks.size();i++){
+                if(tracks.get(i).getRelativeTo()!=theNewTrack.getRelativeTo()){
+                    index2=i;
+                }
+            }
+            tracks.add(index2-1,theNewTrack);
 
-	public File generateSoundFile(){	
+	    if(theNewTrack.getRelativeTo()==tracks.get(0)){
+		OffsetTree.addStartChildren(new TrackNode(theNewTrack));
+	    }else{
+		//go down tree and find your relative :)
+		//tell your relative that you are its child
 		
-	}
-	
-	private byte[] overlapTwoTracks(byte[] a, byte[] b) {
-		
-		boolean aLonger=true;
-		if (a.length<b.length) {
-			aLonger=false;
-		}
-		
-		byte[] out = new byte[a.length];
-		
-		if (aLonger) {
-			try {
+	    }
+        }else{
+            throw new Exception();
+        }
+    }
 
-				for (int i=0; i<a.length; i++) {
-					out[i] = (byte) ((a[i] + b[i]) >> 1);
-				}
+    public int getDuration(){
+       
+    }
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			try {
+    private void saveBytesAsWav(byte[] bytes, String path) throws Exception{
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(byteStream);
+        AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, new File(path));
+    }
+    public void saveScript(){ WriteXML xml=new WriteXML(tracks, path); } //<--XML one
 
-				for (int i=0; i<b.length; i++) {
-					out[i] = (byte) ((a[i] + b[i]) >> 1);
-				}
+    public void saveScriptAsWav(String path){} //<--write out as WAV one
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return out;
-	}
-	
-	private void saveBytesAsWav(byte[] bytes, String path) throws Exception{
-		ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
-		AudioInputStream audioStream = AudioSystem.getAudioInputStream(byteStream);
-		
-		AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, new File(path));
-	}
-
-
-	public void saveScript(){ WriteXML xml=new WriteXML(tracks, path); } //<--XML one
-	public void updateScript(){} //<--write out as WAV one
-
+    //needs to read in XML?
 }
+
+//duration pls
