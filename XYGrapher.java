2e6e9f4f06140c2531a97e79fecd0318c38f49a8
Dallos <maheshsullivan@gmail.com>
2015-03-18 23:28:10 -0400

    Update XYGrapher.java

--- XYGrapher.java
+++ XYGrapher.java
@@ -15,37 +15,37 @@ public class XYGrapher {
     public double yRange(){return myRange;}
 
     public Coordinate getPoint(int pointNum){
-	if(pointNum>=coordinatesForGraph.length){
-	    return null;
-	}else{return coordinatesForGraph[pointNum];}
+        if(pointNum>=coordinatesForGraph.length){
+            return null;
+        }else{return coordinatesForGraph[pointNum];}
     }
 
     public BufferedImage drawGraph(int xPixelStart, int yPixelStart, int pixelsWide, int pixelsHigh){
-        
-	BufferedImage theImage=new BufferedImage(100,(int)xRange(), BufferedImage.TYPE_INT_ARGB);
-	Graphics2D g2 = theImage.createGraphics();
 
-	g2.setColor(new Color(22, 145, 217).darker());
-	
-	Coordinate xy=xyStart();
-	double x=xRange();
-	double y=yRange();
-	int i=1;
-	Coordinate firstPoint=getPoint(0);
-	double xZero = xPixelStart + (firstPoint.getX()-xy.getX())*(pixelsWide/x);
-	double yZero = yPixelStart + (xy.getY()+y-firstPoint.getY())*(pixelsHigh/y);
-	while(getPoint(i)!=null){
-	    Coordinate thePoint=getPoint(i);
-	    double xPixel = xPixelStart + (thePoint.getX()-xy.getX())*(pixelsWide/x);
-	    double yPixel = yPixelStart + (xy.getY()+y-thePoint.getY())*(pixelsHigh/y);
+        BufferedImage theImage=new BufferedImage(pixelsWide,pixelsHigh, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = theImage.createGraphics();
 
-	   
-	    g2.drawLine((int)xZero,(int)yZero,(int)xPixel,(int)yPixel);
-	    xZero=xPixel;
-	    yZero=yPixel;
-	    i++;
-	    
-	}
-	return theImage;
+        g2.setColor(new Color(22, 145, 217).darker());
+
+        Coordinate xy=xyStart();
+        double x=xRange();
+        double y=yRange();
+        int i=1;
+        Coordinate firstPoint=getPoint(0);
+        double xZero = xPixelStart + (firstPoint.getX()-xy.getX())*(pixelsWide/x);
+        double yZero = yPixelStart + (xy.getY()+y-firstPoint.getY())*(pixelsHigh/y);
+        while(getPoint(i)!=null){
+            Coordinate thePoint=getPoint(i);
+            double xPixel = xPixelStart + (thePoint.getX()-xy.getX())*(pixelsWide/x);
+            double yPixel = yPixelStart + (xy.getY()+y-thePoint.getY())*(pixelsHigh/y);
+
+
+            g2.drawLine((int)xZero,(int)yZero,(int)xPixel,(int)yPixel);
+            xZero=xPixel;
+            yZero=yPixel;
+            i++;
+
+        }
+        return theImage;
     }
 }
