32fe9747fec83a446fcdb5b06ed412a022c0fbc8
Dallos <maheshsullivan@gmail.com>
2015-03-18 23:29:57 -0400

    Update TrackNode.java

--- TrackNode.java
+++ TrackNode.java
@@ -2,21 +2,23 @@ import java.util.ArrayList;
 public class TrackNode{
     private Track myTrack;
     public Track getTrackNode(){return myTrack;}
-
     private TrackNode myParent;
     private ArrayList<TrackNode> children;
-
     public TrackNode(Track theNewTrack){
-	myTrack=theNewTrack;
-	if(theNewTrack.getRelativeTo()!=theNewTrack){
-		myParent=new TrackNode(theNewTrack.getRelativeTo());
-	}
-	
-	//gots to set up children boi
+        myTrack=theNewTrack;
+        children=new ArrayList<>();
+        if(theNewTrack.getRelativeTo()!=theNewTrack){
+            myParent=new TrackNode(theNewTrack.getRelativeTo());
+        }else{
+            myParent=this;
+        }
+        //gots to set up children boi
     }
-
     public void addChildren(TrackNode newTrack){
-	children.add(newTrack);
+        children.add(newTrack);
     }
-
+   
+    public int getNumChildren(){return children.size();}
+    public TrackNode getParent(){return myParent;}
+    public ArrayList<TrackNode> getChildren(){return children;}
 }
